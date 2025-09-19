55f59f14204322d9e6b0a64a2bf0d77e4003bb91
Ram-Ram007 <ramajayam2411@gmail.com>
2025-09-19 23:03:42 +0530

    Initial commit

---
+++ Controllers/EmplopyeesController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //local host that the port the app is runing
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmplopyeesController : ControllerBase
+    {
+        private readonly ApplicationDbcontext dbcontext;
+
+        public EmplopyeesController(ApplicationDbcontext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbcontext.Employees.ToList();
+            return Ok(allEmployees);
+        }
+    }
+}
---
+++ Data/ApplicationDbcontext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeAdminPortal.Models.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbcontext : DbContext
+    {
+
+        public ApplicationDbcontext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
